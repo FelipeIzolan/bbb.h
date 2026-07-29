d033d4a04746f14269d14a3f7a470f181b42ff03
Felipe Izolan <80170121+FelipeIzolan@users.noreply.github.com>
2026-07-29 04:32:38 -0300

    update

--- bbb.h
+++ bbb.h
@@ -1,4 +1,4 @@
-#ifndef
+#ifndef BBB_H
 #define BBB_H
 
 #include <stdio.h>
@@ -6,62 +6,72 @@
 typedef struct {
   unsigned char buf;
   unsigned char count;
+  FILE * fp;
 } bbb_t;
 
-size_t freadb(const void *restrict ptr, size_t nbits, bbb_t *restrict ctx, FILE *restrict stream)
-size_t fwriteb(const void *restrict ptr, size_t nbits, bbb_t *restrict ctx, FILE *restrict stream) 
+// int freadb(const void *restrict ptr, size_t nbits, bbb_t *restrict stream)
+int fwriteb(const void *restrict ptr, size_t nbits, bbb_t *restrict stream);
+int fflushb(bbb_t *restrict stream);
 
 #endif // BBB_H
 
 #ifdef BBB_IMPL
-size_t fwriteb(const void *restrict ptr, size_t nbits, bbb_t *restrict ctx, FILE *restrict stream) {
-  if (ptr == NULL && stream == NULL)
+int fwriteb(const void *restrict ptr, size_t nbits, bbb_t *restrict stream) {
+  if (ptr == NULL || stream == NULL)
     return 0;
-
-  static unsigned char buf = 0;
-  static unsigned char count = 0; 
 
   unsigned char * bytes = (char *)ptr;
   size_t length = nbits / 8;
+  
   for (size_t i = 0; i < length; i++) {
     unsigned char byte = bytes[i];
     unsigned char temp = byte;
-    if (count > 0) {
-      buf <<= 8 - count;
-      buf |= byte >> count;
-      temp = buf;
-      buf = byte;
+    if (stream->count > 0) {
+      stream->buf <<= 8 - stream->count;
+      stream->buf |= byte >> stream->count;
+      temp = stream->buf;
+      stream->buf = byte;
     }
-    if (fputc(temp, stream) == EOF)
+    if (fputc(temp, stream->fp) == EOF)
       return EOF;
   }
 
   int remaining = nbits % 8;
   if (remaining > 0) {
     unsigned char byte = bytes[length];  
-    unsigned char diff = 8 - count;
+    unsigned char diff = 8 - stream->count;
     if (remaining <= diff) {
-      buf <<= remaining;
-      buf |= byte;
-      count += remaining;
-      if (count == 8) {
-        if (fputc(buf, stream) == EOF)
+      stream->buf <<= remaining;
+      stream->buf |= byte;
+      stream->count += remaining;
+      if (stream->count == 8) {
+        if (fputc(stream->buf, stream->fp) == EOF)
           return EOF;
-        buf = 0;
-        count = 0;
+        stream->buf = 0;
+        stream->count = 0;
       }
     } else {
-//       unsigned char overflow = remaining - count; 
-//       buf <<= diff;
-//       buf |= byte >> overflow;
-//       if (fputc(buf, stream) == EOF)
-//         return EOF;
-//       buf = byte;
-//       count = overflow;
+      unsigned char overflow = remaining - diff; 
+      stream->buf <<= diff;
+      stream->buf |= byte >> overflow;
+      if (fputc(stream->buf, stream->fp) == EOF)
+        return EOF;
+      stream->buf = byte;
+      stream->count = overflow;
     }
   }
 
-  return 0;
+  return nbits - stream->count;
 }
+
+int fflushb(bbb_t *restrict stream) {
+  stream->buf <<= 8 - stream->count;
+  if (fputc(stream->buf, stream->fp) == EOF)
+    return EOF;
+  stream->buf = 0;
+  stream->count = 0;
+  return fflush(stream->fp);
+}
+
 #endif // BBB_IMPL
 
